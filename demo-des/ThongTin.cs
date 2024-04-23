4ea8e6561eedc2f1bf38284bb610544aa45fbb03
cochecheee <buitien747@gmail.com>
2024-04-23 18:25:17 +0700

    finsh

--- demo-des/ThongTin.cs
+++ demo-des/ThongTin.cs
@@ -52,7 +52,29 @@ namespace demo_des
 
         private void btn_view_Click(object sender, EventArgs e)
         {
-            //lấy thông tin thông qua lbl_username
+            try
+            {
+                //lấy thông tin thông qua lbl_username
+                string username = lbl_username.Text;
+                FirebaseResponse response = client.Get("usersclb/" + username);
+                var tv = response.ResultAs<thanhvien>();
+
+                string decUsername = _3des.Giaima3DES(tv.Username, Key.Key1, Key.Key2, Key.Key3);
+                string decPhone = _3des.Giaima3DES(tv.Phone, Key.Key1, Key.Key2, Key.Key3);
+                string decAddress = _3des.Giaima3DES(tv.Address, Key.Key1, Key.Key2, Key.Key3);
+                string decClb = _3des.Giaima3DES(tv.Clb, Key.Key1, Key.Key2, Key.Key3);
+                string decGender = _3des.Giaima3DES(tv.Gender, Key.Key1, Key.Key2, Key.Key3);
+
+                txt_address.Text = decAddress;
+                txt_gender.Text = decGender;
+                txt_phone.Text = decPhone;
+                txt_club.Text = decClb;
+                txt_username.Text = decUsername;
+            }
+            catch {
+                MessageBox.Show("Sai ở btn view ne..");
+            }
+
         }
         private void layKhoaTuFireBase()
         {
