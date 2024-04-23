c545be8e1b7d531a44250c8e6474d5a1ac9ca019
cochecheee <buitien747@gmail.com>
2024-04-23 17:42:17 +0700

    update new form

---
+++ demo-des/ThongTin.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace demo_des
+{
+    partial class ThongTin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ThongTin));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            lbl_username = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel6 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel7 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel8 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            txt_username = new Guna.UI2.WinForms.Guna2TextBox();
+            txt_phone = new Guna.UI2.WinForms.Guna2TextBox();
+            txt_address = new Guna.UI2.WinForms.Guna2TextBox();
+            txt_gender = new Guna.UI2.WinForms.Guna2TextBox();
+            txt_club = new Guna.UI2.WinForms.Guna2TextBox();
+            btn_view = new Guna.UI2.WinForms.Guna2GradientButton();
+            guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.Khaki;
+            guna2Panel1.Controls.Add(lbl_username);
+            guna2Panel1.Controls.Add(guna2HtmlLabel1);
+            guna2Panel1.Controls.Add(guna2CirclePictureBox1);
+            guna2Panel1.CustomizableEdges = customizableEdges2;
+            guna2Panel1.Dock = DockStyle.Left;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            guna2Panel1.Size = new Size(230, 450);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // lbl_username
+            // 
+            lbl_username.AutoSize = false;
+            lbl_username.BackColor = Color.Transparent;
+            lbl_username.ForeColor = Color.Brown;
+            lbl_username.Location = new Point(24, 245);
+            lbl_username.Name = "lbl_username";
+            lbl_username.Size = new Size(180, 61);
+            lbl_username.TabIndex = 2;
+            lbl_username.Text = "update username";
+            lbl_username.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel1.Location = new Point(84, 160);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(44, 30);
+            guna2HtmlLabel1.TabIndex = 1;
+            guna2HtmlLabel1.Text = "User";
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.Image = (Image)resources.GetObject("guna2CirclePictureBox1.Image");
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(65, 55);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges1;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(90, 80);
+            guna2CirclePictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            guna2CirclePictureBox1.TabIndex = 0;
+            guna2CirclePictureBox1.TabStop = false;
+            // 
+            // guna2HtmlLabel3
+            // 
+            guna2HtmlLabel3.BackColor = Color.Transparent;
+            guna2HtmlLabel3.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel3.Location = new Point(486, 35);
+            guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            guna2HtmlLabel3.Size = new Size(127, 33);
+            guna2HtmlLabel3.TabIndex = 1;
+            guna2HtmlLabel3.Text = "Information";
+            // 
+            // guna2HtmlLabel4
+            // 
+            guna2HtmlLabel4.BackColor = Color.Transparent;
+            guna2HtmlLabel4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            guna2HtmlLabel4.Location = new Point(282, 113);
+            guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            guna2HtmlLabel4.Size = new Size(72, 22);
+            guna2HtmlLabel4.TabIndex = 2;
+            guna2HtmlLabel4.Text = "Username";
+            // 
+            // guna2HtmlLabel5
+            // 
+            guna2HtmlLabel5.BackColor = Color.Transparent;
+            guna2HtmlLabel5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            guna2HtmlLabel5.Location = new Point(282, 168);
+            guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            guna2HtmlLabel5.Size = new Size(47, 22);
+            guna2HtmlLabel5.TabIndex = 2;
+            guna2HtmlLabel5.Text = "Phone";
+            // 
+            // guna2HtmlLabel6
+            // 
+            guna2HtmlLabel6.BackColor = Color.Transparent;
+            guna2HtmlLabel6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            guna2HtmlLabel6.Location = new Point(282, 226);
+            guna2HtmlLabel6.Name = "guna2HtmlLabel6";
+            guna2HtmlLabel6.Size = new Size(57, 22);
+            guna2HtmlLabel6.TabIndex = 2;
+            guna2HtmlLabel6.Text = "Address";
+            // 
+            // guna2HtmlLabel7
+            // 
+            guna2HtmlLabel7.BackColor = Color.Transparent;
+            guna2HtmlLabel7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            guna2HtmlLabel7.Location = new Point(282, 284);
+            guna2HtmlLabel7.Name = "guna2HtmlLabel7";
+            guna2HtmlLabel7.Size = new Size(53, 22);
+            guna2HtmlLabel7.TabIndex = 2;
+            guna2HtmlLabel7.Text = "Gender";
+            // 
+            // guna2HtmlLabel8
+            // 
+            guna2HtmlLabel8.BackColor = Color.Transparent;
+            guna2HtmlLabel8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            guna2HtmlLabel8.Location = new Point(282, 340);
+            guna2HtmlLabel8.Name = "guna2HtmlLabel8";
+            guna2HtmlLabel8.Size = new Size(69, 22);
+            guna2HtmlLabel8.TabIndex = 2;
+            guna2HtmlLabel8.Text = "Your club";
+            // 
+            // txt_username
+            // 
+            txt_username.BorderRadius = 10;
+            txt_username.CustomizableEdges = customizableEdges4;
+            txt_username.DefaultText = "";
+            txt_username.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txt_username.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txt_username.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txt_username.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txt_username.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_username.Font = new Font("Segoe UI", 9F);
+            txt_username.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_username.Location = new Point(381, 103);
+            txt_username.Margin = new Padding(3, 4, 3, 4);
+            txt_username.Name = "txt_username";
+            txt_username.PasswordChar = '\0';
+            txt_username.PlaceholderText = "";
+            txt_username.ReadOnly = true;
+            txt_username.SelectedText = "";
+            txt_username.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            txt_username.Size = new Size(339, 32);
+            txt_username.TabIndex = 3;
+            // 
+            // txt_phone
+            // 
+            txt_phone.BorderRadius = 10;
+            txt_phone.CustomizableEdges = customizableEdges6;
+            txt_phone.DefaultText = "";
+            txt_phone.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txt_phone.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txt_phone.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txt_phone.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txt_phone.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_phone.Font = new Font("Segoe UI", 9F);
+            txt_phone.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_phone.Location = new Point(381, 158);
+            txt_phone.Margin = new Padding(3, 4, 3, 4);
+            txt_phone.Name = "txt_phone";
+            txt_phone.PasswordChar = '\0';
+            txt_phone.PlaceholderText = "";
+            txt_phone.ReadOnly = true;
+            txt_phone.SelectedText = "";
+            txt_phone.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            txt_phone.Size = new Size(339, 32);
+            txt_phone.TabIndex = 3;
+            // 
+            // txt_address
+            // 
+            txt_address.BorderRadius = 10;
+            txt_address.CustomizableEdges = customizableEdges8;
+            txt_address.DefaultText = "";
+            txt_address.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txt_address.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txt_address.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txt_address.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txt_address.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_address.Font = new Font("Segoe UI", 9F);
+            txt_address.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_address.Location = new Point(381, 216);
+            txt_address.Margin = new Padding(3, 4, 3, 4);
+            txt_address.Name = "txt_address";
+            txt_address.PasswordChar = '\0';
+            txt_address.PlaceholderText = "";
+            txt_address.ReadOnly = true;
+            txt_address.SelectedText = "";
+            txt_address.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            txt_address.Size = new Size(339, 32);
+            txt_address.TabIndex = 3;
+            // 
+            // txt_gender
+            // 
+            txt_gender.BorderRadius = 10;
+            txt_gender.CustomizableEdges = customizableEdges10;
+            txt_gender.DefaultText = "";
+            txt_gender.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txt_gender.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txt_gender.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txt_gender.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txt_gender.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_gender.Font = new Font("Segoe UI", 9F);
+            txt_gender.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_gender.Location = new Point(381, 274);
+            txt_gender.Margin = new Padding(3, 4, 3, 4);
+            txt_gender.Name = "txt_gender";
+            txt_gender.PasswordChar = '\0';
+            txt_gender.PlaceholderText = "";
+            txt_gender.ReadOnly = true;
+            txt_gender.SelectedText = "";
+            txt_gender.ShadowDecoration.CustomizableEdges = customizableEdges11;
+            txt_gender.Size = new Size(339, 32);
+            txt_gender.TabIndex = 3;
+            // 
+            // txt_club
+            // 
+            txt_club.BorderRadius = 10;
+            txt_club.CustomizableEdges = customizableEdges12;
+            txt_club.DefaultText = "";
+            txt_club.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txt_club.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txt_club.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txt_club.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txt_club.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_club.Font = new Font("Segoe UI", 9F);
+            txt_club.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txt_club.Location = new Point(381, 330);
+            txt_club.Margin = new Padding(3, 4, 3, 4);
+            txt_club.Name = "txt_club";
+            txt_club.PasswordChar = '\0';
+            txt_club.PlaceholderText = "";
+            txt_club.ReadOnly = true;
+            txt_club.SelectedText = "";
+            txt_club.ShadowDecoration.CustomizableEdges = customizableEdges13;
+            txt_club.Size = new Size(339, 32);
+            txt_club.TabIndex = 3;
+            // 
+            // btn_view
+            // 
+            btn_view.BorderRadius = 20;
+            btn_view.CustomizableEdges = customizableEdges14;
+            btn_view.DisabledState.BorderColor = Color.DarkGray;
+            btn_view.DisabledState.CustomBorderColor = Color.DarkGray;
+            btn_view.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btn_view.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btn_view.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btn_view.FillColor = Color.FromArgb(255, 255, 128);
+            btn_view.FillColor2 = Color.FromArgb(255, 192, 192);
+            btn_view.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_view.ForeColor = Color.Black;
+            btn_view.Location = new Point(525, 381);
+            btn_view.Name = "btn_view";
+            btn_view.ShadowDecoration.CustomizableEdges = customizableEdges15;
+            btn_view.Size = new Size(195, 35);
+            btn_view.TabIndex = 4;
+            btn_view.Text = "View";
+            btn_view.Click += btn_view_Click;
+            // 
+            // ThongTin
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleTurquoise;
+            ClientSize = new Size(817, 450);
+            Controls.Add(btn_view);
+            Controls.Add(txt_club);
+            Controls.Add(txt_gender);
+            Controls.Add(txt_address);
+            Controls.Add(txt_phone);
+            Controls.Add(txt_username);
+            Controls.Add(guna2HtmlLabel8);
+            Controls.Add(guna2HtmlLabel7);
+            Controls.Add(guna2HtmlLabel5);
+            Controls.Add(guna2HtmlLabel6);
+            Controls.Add(guna2HtmlLabel4);
+            Controls.Add(guna2HtmlLabel3);
+            Controls.Add(guna2Panel1);
+            Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            Name = "ThongTin";
+            Text = "Thông tin cá nhân";
+            Load += ThongTin_Load;
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel6;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel7;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel8;
+        private Guna.UI2.WinForms.Guna2TextBox txt_username;
+        private Guna.UI2.WinForms.Guna2TextBox txt_phone;
+        private Guna.UI2.WinForms.Guna2TextBox txt_address;
+        private Guna.UI2.WinForms.Guna2TextBox txt_gender;
+        private Guna.UI2.WinForms.Guna2TextBox txt_club;
+        private Guna.UI2.WinForms.Guna2GradientButton btn_view;
+        public Guna.UI2.WinForms.Guna2HtmlLabel lbl_username;
+    }
+}
---
+++ demo-des/ThongTin.cs
@@ -0,0 +1,63 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo_des
+{
+    public partial class ThongTin : Form
+    {
+        public ThongTin()
+        {
+            InitializeComponent();
+        }
+        //kết nối project với firebase
+        IFirebaseConfig config = new FirebaseConfig()
+        {
+            AuthSecret = "C4n3cmu3NJVytNiNwmOt1lSiyknFm2GakZXJXsr5",
+            BasePath = "https://fir-des-647b5-default-rtdb.firebaseio.com/",
+        };
+        IFirebaseClient client;
+        key Key;
+
+        private void ThongTin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new FirebaseClient(config);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối firebase..");
+            }
+
+            try
+            {
+                layKhoaTuFireBase();
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi từ lấy khóa..");
+            }
+        }
+
+        private void btn_view_Click(object sender, EventArgs e)
+        {
+            //lấy thông tin thông qua lbl_username
+        }
+        private void layKhoaTuFireBase()
+        {
+            FirebaseResponse response = client.Get("key/");
+            Key = response.ResultAs<key>();
+        }
+    }
+}
